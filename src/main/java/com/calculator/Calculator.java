d6b030e9be0c20480ef67dc8476c712ababa63a5
BukkaNikhilSai <bukkanikhilsai@gmail.com>
2020-05-08 21:47:24 +0530

    menu driven

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,15 +1,39 @@
 package com.calculator;
 
+import java.util.*;
+
 public class Calculator {
 
 	public static void main(String[] args){
-		System.out.println("6 + 3 = " + add(9, 3));
-		System.out.println("6 + 3 = " + add(6, 3));
-		System.out.println("6 - 3 = " + subtract(6,3));
-		System.out.println("6 * 3 = " + multiply(6,3));
-		System.out.println("6 - 3 = " + subtract(6,3));
-		System.out.println("6 * 3 = " + multiply(6,3));
-		System.out.println("6 / 3 = " + divide(6,3));
+		int no1,no2,operation;
+		Scanner reader = new Scanner(System.in);
+
+		while(true){
+			System.out.println("1.Addition\n2.Subtraction\n3.Muliplication\n4.Division\n5.Exit");
+			System.out.print("Choose the operation: ");
+			operation = reader.nextInt();
+			if(operation==5){System.out.println("Have a Nice Day :)");break;}
+			else{
+							System.out.print("Enter the numbers with a space: ");
+							no1 = reader.nextInt();
+							no2 = reader.nextInt();
+							switch(operation){
+								case 1:
+									System.out.println("Sum of the numbers is "+add(no1,no2));
+									break;
+								case 2:
+									System.out.println("Difference of the numbers is "+subtract(no1,no2));
+									break;
+								case 3:
+									System.out.println("Product of the numbers is "+multiply(no1,no2));
+									break;
+								case 4:
+									System.out.println("cofficient of the numbers is "+divide(no1,no2));
+									break;
+							}
+
+						}
+		}
 	}
 
 	public static int add(int a, int b) {
