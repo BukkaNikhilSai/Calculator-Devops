7193fb136a462e8c9d20b8b958381510a0f78b79
BukkaNikhilSai <bukkanikhilsai@gmail.com>
2020-05-08 21:31:34 +0530

    changes in main function

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -3,7 +3,10 @@ package com.calculator;
 public class Calculator {
 
 	public static void main(String[] args){
+		System.out.println("6 + 3 = " + add(9, 3));
 		System.out.println("6 + 3 = " + add(6, 3));
+		System.out.println("6 - 3 = " + subtract(6,3));
+		System.out.println("6 * 3 = " + multiply(6,3));
 		System.out.println("6 - 3 = " + subtract(6,3));
 		System.out.println("6 * 3 = " + multiply(6,3));
 		System.out.println("6 / 3 = " + divide(6,3));
